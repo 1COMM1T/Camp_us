c016d009c5e9aec91246297c166106a899033b1c
PCH <paekrng7@naver.com>
2024-07-17 15:21:50 +0900

    chore: 캠프어스 네이밍 수정 및 재조정

--- src/main/java/com/commit/campus/CampReserveApplication.java
+++ src/main/java/com/commit/campus/CampReserveApplication.java
@@ -1,4 +1,4 @@
-package com.commit.campus;
+package com.comm1t.Camp_Us;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/commit/campus/CampReserveApplicationTests.java
+++ src/test/java/com/commit/campus/CampReserveApplicationTests.java
@@ -1,4 +1,8 @@
+<<<<<<<< HEAD:src/test/java/com/commit/campus/CampReserveApplicationTests.java
 package com.commit.campus;
+========
+package com.comm1t.Camp_Us;
+>>>>>>>> 2600155 (chore: 캠프어스 네이밍 수정 및 재조정):src/test/java/com/comm1t/Camp_Us/CampReserveApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
