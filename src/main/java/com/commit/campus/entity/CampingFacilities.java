8e485c407e6f0618b69d42b487a26f3ee188a80a
bucky1005 <null00007@naver.com>
2024-08-04 23:16:53 +0900

    style: 테이블 이름 명시
    Fixes: #106, #107

--- src/main/java/com/commit/campus/entity/CampingFacilities.java
+++ src/main/java/com/commit/campus/entity/CampingFacilities.java
@@ -11,6 +11,7 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
+@Table(name = "camping_facilities")
 //@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CampingFacilities {
     @Id
